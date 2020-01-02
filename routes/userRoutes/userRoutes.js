1eb77ea3ba534a75b04c6eaa4a8ec3adbfc2eed0
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 13:32:33 -0500

    amc-1

--- routes/userRoutes/userRoutes.js
+++ routes/userRoutes/userRoutes.js
@@ -6,7 +6,7 @@ const Users = require('../../data/models/users/usersModel');
 router.get('/', (req, res) => {
     Users.get()
         .then(res => {
-            console.log(res)
+            res.status(200).json(res);
         })
         .catch(err => {
             console.log(err)
