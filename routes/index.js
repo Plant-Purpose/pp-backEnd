70ef3bd3ee1d7b7d119a95af060eff609ae621d9
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 13:41:29 -0500

    Users Fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes/userRoutes');
 
+router.use(express.json())
+
 router.use('/users', userRoutes);
 
 module.exports = router;
--- routes/userRoutes/userRoutes.js
+++ routes/userRoutes/userRoutes.js
@@ -5,8 +5,8 @@ const Users = require('../../data/models/users/usersModel');
 
 router.get('/', async (req, res) => {
     await Users.get()
-        .then(res => {
-            res.status(200).json(res);
+        .then(users => {
+            res.status(200).json(users);
         })
         .catch(err => {
             console.log(err)
@@ -15,9 +15,9 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     await Users.getById(req.params.id)
-        .then(res => {
+        .then(user => {
             console.log(res)
-            res.status(200).json(res)
+            res.status(200).json(user)
         })
         .catch(err => {
             console.log(err)
