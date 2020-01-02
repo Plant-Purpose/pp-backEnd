8cf1438e2914dba511d489e45f38a6080bacd845
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 14:42:39 -0500

    reg user works

--- middleware/auth/auth.js
+++ middleware/auth/auth.js
@@ -4,12 +4,13 @@ const jwt = require('jsonwebtoken');
 
 const Users = require('../../data/models/users/usersModel');
 
+// api/auth/register
 router.post('/register', (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 7);
     user.password = hash;
 
-    Users.add(user)
+    Users.insert(user)
         .then(({ id }) => {
             res.status(201).json({ status: 201, message: id });
         })
@@ -17,7 +18,6 @@ router.post('/register', (req, res) => {
             res.status(500).json({ error: error, message: 'registration failed' });
         });
 });
-
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
+const authRoutes = require('../middleware/auth/auth');
 const userRoutes = require('./userRoutes/userRoutes');
 const userPlantRoutes = require('./userPlantRoutes/userPlantRoutes');
 
 router.use('/users', userRoutes, userPlantRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
