e2dcaa4eb5e361d138980fdd50467fd82c7c34a7
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 11:32:56 -0500

    ready for deploy

--- data/migrations/20200102111634_userPlants.js
+++ data/migrations/20200102111634_userPlants.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('userPlants', tbl => {
+        // ID
+        tbl.increments();
+
+        // FK
+        tbl.integer('plant_id');
+
+        tbl.string('create_at');
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('userPlants');
 };
---
+++ data/migrations/20200102111648_tasks.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('tasks', tbl => {
+        // ID 
+        tbl.increments();
+
+        // String
+        tbl.string('title').notNullable();
+        tbl.string('task').notNullable();
+        tbl.string('deadline').notNullable();
+        tbl.string('created_at');
+
+        // Int
+        tbl.int('user_id').notNullable();
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('tasks');
+};
