b5c3d2eca35ae56e1e3f3483f842f87d96c8039c
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 13:43:02 -0500

    Index Fix

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes/userRoutes');
 
-router.use(express.json())
-
 router.use('/users', userRoutes);
 
 module.exports = router;
