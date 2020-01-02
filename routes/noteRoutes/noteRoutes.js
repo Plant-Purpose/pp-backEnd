564df1763bbe3d91919411f9413ee778d08d6b8b
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 16:52:55 -0500

    PUT update

--- routes/noteRoutes/noteRoutes.js
+++ routes/noteRoutes/noteRoutes.js
@@ -45,39 +45,30 @@ router.put('/:plant_id/:note_id', (req, res) => {
     const { plant_id, note_id } = req.params;
     const changes = req.body;
 
-    Notes.findById(plant_id, note_id)
-        .then(notes => {
-            if (notes) {
-                Notes.update(changes, plant_id, note_id)
-                    .then(updatedNote => {
-                        res.json(updatedNote);
-                    })
-            } else {
-                res.status(404).json({ message: "Could not find note with given id." });
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: "Failed to update notes." });
-        });
+    Notes.update(note_id, changes)
 });
 
-router.delete('/:plant_id/:note_id', (req, res) => {
-    const { plant_id, note_id } = req.params;
-
-    Notes.remove(plant_id, note_id)
-        .then(deleted => {
-            if (deleted) {
-                res.json({ removed: deleted });
-            } else {
-                res.status(404).json({ message: "Could not find note with given id." })
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: "Failed to delete note." })
-        })
+r.then(update => {
+    res.status(200).json(update);
 })
+    .catch(err => {
+        console.log(err);
+    }); outer.delete('/:plant_id/:note_id', (req, res) => {
+        const { plant_id, note_id } = req.params;
+
+        Notes.remove(plant_id, note_id)
+            .then(deleted => {
+                if (deleted) {
+                    res.json({ removed: deleted });
+                } else {
+                    res.status(404).json({ message: "Could not find note with given id." })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ message: "Failed to delete note." })
+            })
+    })
 
 
 module.exports = router;
