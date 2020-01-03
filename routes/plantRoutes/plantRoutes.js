e11ccdcb07ee7e490e5d67caf20ba6f4f986bdb0
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-02 21:30:39 -0500

    debug api call

--- routes/plantRoutes/plantRoutes.js
+++ routes/plantRoutes/plantRoutes.js
@@ -6,15 +6,22 @@ const router = require('express').Router();
 const ENDPOINT = process.env.URL;
 const SECRET = process.env.SECRET;
 
-router.get('/browse', (req, res) => {
-    axios.get(ENDPOINT + SECRET)
-        .then(plants => {
-            console.log(plants)
-            res.status(200).json(plants);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+router.get('/browse', async (req, res) => {
+    try {
+        const plants = await axios.get(ENDPOINT + SECRET);
+
+        res.status(200).json(plants);
+    } catch (err) {
+        console.log(err);
+    }
+    // axios.get(ENDPOINT + SECRET)
+    //     .then(plants => {
+
+    //         res.status(200).json(plants);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
 });
 
 router.get('/browse/:id', (req, res) => {
