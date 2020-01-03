e0934bdb116a8fc4b5b461234a90bed1079e13d9
Trip1701 <bj2336@email.vccs.edu>
2020-01-03 09:10:31 -0500

    Twilio Implementation

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ const taskRoutes = require('./taskRoutes/taskRoutes');
 const noteRoutes = require('./noteRoutes/noteRoutes');
 const plantRoutes = require('./plantRoutes/plantRoutes');
 
+const smsRoutes = require('../sms/sms_notifications');
+
 router.use('/users', userRoutes, userPlantRoutes, taskRoutes);
 router.use('/plants', plantRoutes)
 router.use('/auth', authRoutes);
 router.use('/notes', noteRoutes);
+router.use('/sms', smsRoutes);
 
 module.exports = router;
---
+++ sms/sms_notifications.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const accountSid = process.env.SID;
+const authToken = process.env.TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const router = require('express').Router();
+
+router.post('/schedule', (req, res) => {
+
+    setTimeout(() => {
+        client.messages
+        .create({
+          body: req.body.task,
+          from: process.env.PHONE,
+          to: req.body.phone
+        })
+        .then(() => res.status(200).json({ message: "Reminder Will Be Sent In" + ((req.body.time / 1000) / 60 + minutes) }))
+        .catch(err => console.log(err));
+    }, req.body.time);
+
+});
+
+module.exports = router;
